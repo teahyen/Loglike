91b8af11034cba34f4eed43a3494ce1dbb6c00b6
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-04-09 23:27:10 +0900

    맵 만들기 맵 버그 수정

--- Assets/05.Script/MapScript.cs
+++ Assets/05.Script/MapScript.cs
@@ -7,28 +7,36 @@ public class MapScript : MonoBehaviour
     [SerializeField]
     private GameObject Player;
     private int RDMax;
+    [SerializeField]
+    [Header("보스방이 나오는 시점")]
+    private int BosRoomTime;
     public void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
         {
             RDMax = Random.Range(0, 101);
-            if (RDMax >= 5 && RDMax <= 0)
+            if (RDMax >0&& RDMax <5 &&BosRoomTime <=0)
             {
                 Debug.Log("보스방");
             }
-            else if (RDMax < 5 && RDMax >= 90)
-            {
-                Debug.Log("일반방");
-            }
-            else if (RDMax < 90 && RDMax >= 99)
-            {
-                Debug.Log("아이템방");
-            }
-            else if (RDMax < 99 && RDMax >= 100)
+            else if (RDMax < 99)
             {
                 Debug.Log("비밀방");
             }
+            else
+            {
+                Debug.Log("일반방");
+            }
+            //else if (RDMax < 90 && RDMax >= 99)
+            //{
+            //    Debug.Log("아이템방");
+            //}
+            //else if (RDMax < 99 && RDMax >= 100)
+            //{
+            //    Debug.Log("비밀방");
+            //}
             Debug.Log(RDMax);
+            BosRoomTime--;
         }
     }
 }
