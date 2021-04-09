3f985f96e14a8512297a3975e181f6e9c4fe4551
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-04-09 22:04:28 +0900

    일단 맵추가+ 이동 구현 문 구현 필요

--- Assets/05.Script/MapScript.cs
+++ Assets/05.Script/MapScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MapScript : MonoBehaviour
 {
@@ -15,22 +14,18 @@ public class MapScript : MonoBehaviour
             RDMax = Random.Range(0, 101);
             if (RDMax >= 5 && RDMax <= 0)
             {
-                //SceneManager.LoadScene("BossScenes");
                 Debug.Log("보스방");
             }
             else if (RDMax < 5 && RDMax >= 90)
             {
-                //SceneManager.LoadScene("Stage");
                 Debug.Log("일반방");
             }
             else if (RDMax < 90 && RDMax >= 99)
             {
-                //SceneManager.LoadScene("GoldRoomScenes");
                 Debug.Log("아이템방");
             }
             else if (RDMax < 99 && RDMax >= 100)
             {
-                //SceneManager.LoadScene("SecretScenes");
                 Debug.Log("비밀방");
             }
             Debug.Log(RDMax);
