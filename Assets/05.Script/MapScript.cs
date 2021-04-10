5d5034ffbb9767e743849947bbe6067f96b890a7
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-04-10 13:28:38 +0900

    맵이동 스크립트 수정

--- Assets/05.Script/MapScript.cs
+++ Assets/05.Script/MapScript.cs
@@ -6,10 +6,13 @@ public class MapScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject Player;
+    [SerializeField][Header("0 일반방, 1 보스방, 2 비밀방")]
+    private List<GameObject> room = new List<GameObject>();
     private int RDMax;
     [SerializeField]
     [Header("보스방이 나오는 시점")]
     private int BosRoomTime;
+
     public void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
@@ -18,23 +21,18 @@ public class MapScript : MonoBehaviour
             if (RDMax >0&& RDMax <5 &&BosRoomTime <=0)
             {
                 Debug.Log("보스방");
+                Player.transform.position =room[1].transform.position;
             }
             else if (RDMax > 99)
             {
                 Debug.Log("비밀방");
+                Player.transform.position = room[2].transform.position;
             }
             else
             {
                 Debug.Log("일반방");
+                Player.transform.position = room[0].transform.position;
             }
-            //else if (RDMax < 90 && RDMax >= 99)
-            //{
-            //    Debug.Log("아이템방");
-            //}
-            //else if (RDMax < 99 && RDMax >= 100)
-            //{
-            //    Debug.Log("비밀방");
-            //}
             Debug.Log(RDMax);
             BosRoomTime--;
         }
